d40cc9f562e01fc19577b1e46a1464a3ddb07bb3
Dryshshuk <143707487+Dryshshuk@users.noreply.github.com>
2023-09-04 18:17:46 +0300

    Add files via upload

---
+++ personclass.rb
@@ -0,0 +1,25 @@
+﻿class PersonRepository
+  def initialize
+    @people = []
+  end
+
+  def add_person(person)
+    @people << person
+  end
+
+  def remove_person(person)
+    @people.delete(person)
+  end
+
+  def find_by_name(name)
+    @people.find { |person| person.name == name }
+  end
+
+  def find_by_attribute(attribute, value)
+    @people.select { |person| person.send(attribute) == value }
+  end
+
+  def all_people
+    @people
+  end
+end
